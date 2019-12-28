e9579bdcea9e06619d5e2b6e23991834279dbc7d
Sumit Borhade <sumitborhade@gmail.com>
2019-12-28 17:20:12 +0800

    Initial commit for graphics problem

---
+++ src/main/java/com/example/graphics/model/Point.java
@@ -0,0 +1,23 @@
+package com.example.graphics.model;
+
+public class Point {
+
+	private int x;
+
+	private int y;
+	
+	public Point(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+}
